91d4679f7b1e62bc689fcf23beedea591fa7ea69
JS <127316906+whygothisway@users.noreply.github.com>
2023-08-24 17:47:29 +0800

    log 10 function (#45)
    Co-authored-by: BluCloudEngineer <83106283+BluCloudEngineer@users.noreply.github.com>

--- application.py
+++ application.py
@@ -36,6 +36,10 @@ def calculate():
     if operation == "custom_log_base":
         base = float(request.form["number_2"])
         result = math.log(number_1, base)
+    
+    if operation == "log10":
+        result = math.log10(number_1)
+
     if operation == "modulus":
         divider = float(request.form["number_2"])
         result = math.modf(number_1, divider)

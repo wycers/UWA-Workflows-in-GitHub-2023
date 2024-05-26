bfaeba9a3081aa83c295b013829a4b9aa6938298
Pritam Shrestha <shresthapritam7@gmail.com>
2024-05-26 14:37:03 +0800

    feature/issue-39/Add subtraction feature (#46)
    Co-authored-by: Pritam Suwal Shrestha <pshre@Pritams-MacBook-Air.local>

--- application.py
+++ application.py
@@ -51,6 +51,10 @@ def calculate():
     if operation == "celsius_to_fahrenheit":
         result = float(number_1 * 9 / 5 + 32)
 
+    if operation == 'subtraction':
+        number_2 = float(request.form["number_2"])
+        result = number_1 - number_2
+
     return render_template("index.html", result=result)
 
 

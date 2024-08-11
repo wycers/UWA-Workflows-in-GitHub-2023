f972841c0972ede38be7a6cebd8a394d765738d8
BluCloudEngineer <83106283+BluCloudEngineer@users.noreply.github.com>
2024-08-11 17:42:40 +0800

    Feature/presentation (#70)
    * Added addition code
    
    * Added addition unit tests

--- application.py
+++ application.py
@@ -42,40 +42,9 @@ def calculate():
     number_1 = float(request.form["number_1"])
 
     # Perform mathematical operations
-    if operation == "celsius_to_fahrenheit":
-        fahrenheit = float(number_1 * 9 / 5 + 32)
-        result = f"{fahrenheit} °F"
-
-    if operation == "celsius_to_kelvin":
-        result = float(number_1) + 273.15
-
-    if operation == "to_hexadecimal":
-        result = hex(int(number_1)).replace("0x", "").upper()
-
-    if operation == "custom_log_base":
-        base = float(request.form["number_2"])
-        result = math.log(number_1, base)
-
-    if operation == "factorial":
-        result = math.factorial(int(number_1))
-
-    if operation == "log10":
-        result = math.log10(number_1)
-
-    if operation == "modulus":
-        divider = float(request.form["number_2"])
-        result = number_1 % divider
-
-    if operation == "multiplication":
+    if operation == "addition":
         number_2 = float(request.form["number_2"])
-        result = number_1 * number_2
-
-    if operation == "sqrt":
-        result = f"± {math.sqrt(number_1)}"
-
-    if operation == "subtraction":
-        number_2 = float(request.form["number_2"])
-        result = number_1 - number_2
+        result = number_1 + number_2
 
     return render_template("index.html", result=result)
 

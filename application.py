f85ad9d9c51c10b712a5ac4745c97669a9a225e1
wycer <wycers@gmail.com>
2025-03-27 17:52:55 +0800

    addition!

--- application.py
+++ application.py
@@ -77,6 +77,12 @@ def calculate():
         number_2 = float(request.form["number_2"])
         result = number_1 - number_2
 
+    if operation == "addition":
+        number_1 = float(request.form["number_1"])
+        number_2 = float(request.form["number_2"])
+        print(number_1, number_2)
+        result = number_1 + number_2
+
     return render_template("index.html", result=result)
 
 
